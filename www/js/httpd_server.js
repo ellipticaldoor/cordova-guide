1434030b03c1509d26de41c5af6a5978f9bbf151
Miguel Dorta <dortamiguel@gmail.com>
2015-10-30 20:47:50 +0000

    cordova local-webserver plugin added

---
+++ www/js/httpd_server.js
@@ -0,0 +1,60 @@
+var httpd = null;
+function onLoad() {
+	document.addEventListener("deviceready", onDeviceReady, false);
+}
+function onDeviceReady() {
+	httpd = ( cordova && cordova.plugins && cordova.plugins.CorHttpd ) ? cordova.plugins.CorHttpd : null;
+
+	startServer("static");
+}
+function updateStatus() {
+	document.getElementById('location').innerHTML = "document.location.href: " + document.location.href;
+	if( httpd ) {
+		httpd.getURL(function(url){
+			if(url.length > 0) {
+				document.getElementById('url').innerHTML = "server is up: <a href='" + url + "' target='_blank'>" + url + "</a>";
+			} else {
+				document.getElementById('url').innerHTML = "server is down.";
+			}
+		});
+		httpd.getLocalPath(function(path){
+			document.getElementById('localpath').innerHTML = "<br/>localPath: " + path;
+		});
+	} else {
+		alert('CorHttpd plugin not available/ready.');
+	}
+}
+function startServer( wwwroot ) {
+	if ( httpd ) {
+		httpd.getURL(function(url){
+			if(url.length > 0) {
+				document.getElementById('url').innerHTML = "server is up: <a href='" + url + "' target='_blank'>" + url + "</a>";
+			} else {
+				httpd.startServer({
+					'www_root' : wwwroot,
+					'port' : 8888
+				}, function( url ){
+					//document.getElementById('url').innerHTML = "server is started: <a href='" + url + "' target='_blank'>" + url + "</a>";
+					updateStatus();
+				}, function( error ){
+					document.getElementById('url').innerHTML = 'failed to start server: ' + error;
+				});
+			}
+			
+		},function(){});
+	} else {
+		alert('CorHttpd plugin not available/ready.');
+	}
+}
+function stopServer() {
+	if ( httpd ) {
+		httpd.stopServer(function(){
+			//document.getElementById('url').innerHTML = 'server is stopped.';
+			updateStatus();
+		},function( error ){
+			document.getElementById('url').innerHTML = 'failed to stop server' + error;
+		});
+	} else {
+		alert('CorHttpd plugin not available/ready.');
+	}
+}
