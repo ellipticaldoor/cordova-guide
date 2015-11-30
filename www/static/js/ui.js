face2c7bfa213db4f2f17bf3e02b446846a26ead
Miguel Dorta <dortamiguel@gmail.com>
2015-11-30 01:13:21 +0000

    added different pages on the app

--- www/static/js/ui.js
+++ www/static/js/ui.js
@@ -18,29 +18,38 @@ function setUI() {
   var hotspot_content = document.getElementById('hotspot_content');
   var howto_content = document.getElementById('howto_content');
 
+	// other
+	var title = document.getElementById('title');
+
+	// Initiate app
+	home_content.style.display = 'block';
 
   home_link.addEventListener('click', function() {
   	current_content = 'home_link';
   	hide_content();
     home_content.style.display = 'block';
+		title.innerHTML = 'Home';
   });
 
   stream_link.addEventListener('click', function() {
   	current_content = 'stream_link';
   	hide_content();
     stream_content.style.display = 'block';
+		title.innerHTML = 'Stream';
   });
 
   hotspot_link.addEventListener('click', function() {
   	current_content = 'hotspot_link';
   	hide_content();
     hotspot_content.style.display = 'block';
+		title.innerHTML = 'Wifi Hotspot';
   });
 
   howto_link.addEventListener('click', function() {
   	current_content = 'howto_link';
   	hide_content();
     howto_content.style.display = 'block';
+		title.innerHTML = 'Howto';
   });
 
   function hide_content() {
@@ -48,6 +57,8 @@ function setUI() {
     stream_content.style.display = 'none';
     hotspot_content.style.display = 'none';
     howto_content.style.display = 'none';
+
+		$( '.mdl-layout__drawer, .mdl-layout__obfuscator' ).removeClass( 'is-visible' );
   }
 
 
